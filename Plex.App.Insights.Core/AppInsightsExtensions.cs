60811a5ff826c13105cf37a8ca2f3f60dbeb774d
Tom Lee <nam.brucelee15987@gmail.com>
2024-01-22 13:44:32 +0700

    Initial commit

---
+++ Plex.App.Insights.Core/AppInsightsExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Plex.App.Insights.Core;
+
+public static class AppInsightsExtensions
+{
+    public static IServiceCollection AddApplicationInsightsTelemetryAndProfiler(
+                                    this IServiceCollection services,
+                                    string cloudRoleName = "",
+                                    string cloudRoleInstance = "")
+    {
+        if (!string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING")))
+        {
+            services.AddApplicationInsightsTelemetry(); // Add this line of code to enable Application Insights.
+            services.AddServiceProfiler(); // Add this line of code to enable Profiler
+
+            if (!string.IsNullOrWhiteSpace(cloudRoleName) && !string.IsNullOrWhiteSpace(cloudRoleInstance))
+                services.AddSingleton<ITelemetryInitializer>(sp => new PlexCloudRoleNameInitializer(cloudRoleName, cloudRoleInstance));
+        }
+        return services;
+    }
+}
---
+++ Plex.App.Insights.Core/PlexCloudRoleNameInitializer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Plex.App.Insights.Core;
+
+public class PlexCloudRoleNameInitializer : ITelemetryInitializer
+{
+    public PlexCloudRoleNameInitializer()
+    {
+        CloudRoleName = "";
+        CloudRoleInstance = "";
+    }
+    public PlexCloudRoleNameInitializer(string cloudRoleName, string cloudRoleInstance)
+    {
+        CloudRoleName = cloudRoleName;
+        CloudRoleInstance = cloudRoleInstance;
+    }
+    public string? CloudRoleName { get; }
+    public string? CloudRoleInstance { get; }
+    public void Initialize(ITelemetry telemetry)
+    {
+        if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName))
+        {
+            telemetry.Context.Cloud.RoleName = Environment.GetEnvironmentVariable("CLOUD_ROLE_NAME") ?? CloudRoleName;
+            telemetry.Context.Cloud.RoleInstance = Environment.GetEnvironmentVariable("CLOUD_ROLE_INSTANCE") ?? CloudRoleInstance;
+        }
+    }
+}
+
